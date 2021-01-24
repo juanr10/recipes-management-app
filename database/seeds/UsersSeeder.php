c136ac46e61c5057c3ba1c5066a05b2f3933a037
juanr10 <argudojuan7@gmail.com>
2021-01-24 19:32:24 +0100

    users & recipe_categories seeder added

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'url' => 'https://github.com/juanr10',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Juan',
+            'email' => 'juan@juan.com',
+            'password' => bcrypt('password'),
+            'url' => 'https://github.com/juanr10',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
